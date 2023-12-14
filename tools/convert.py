d6c208ae151493ac8ae8b0433face5d1f0bb6e67
Dhammiko Bodhi Avatara <100108392+Dhaboav@users.noreply.github.com>
2023-12-15 01:05:30 +0700

    Add progress bar
    Adding progress bar feature and improve readabilty

--- tools/convert.py
+++ tools/convert.py
@@ -1,43 +1,44 @@
 import xml.etree.ElementTree as ET
 import os
+import sys
 
 
 class ConvertXML2YOLO:
-    def __init__(self, xml_labels_path, class_mapping):
-        self.xml_folder = xml_labels_path
-        self.class_mapping = class_mapping
+    def __init__(self, xml_folder_path, class_mapping):
+        self.xml_folder  = xml_folder_path
+        self.classes     = class_mapping
         self.yolo_folder = os.path.join('runs', 'convert')
-        self.folder_name = 'result'
-        self.base_folder_path = self.check_folder()
+        self.folder_name = 'exp'
+        self.folder_path = self.checking_folder_name()
     
+    # Directory Stuff
+    def checking_folder_name(self):
+        folder = os.path.join(self.yolo_folder, self.folder_name)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+            return folder
 
-    def check_folder(self):
-        folder_path = os.path.join(self.yolo_folder, self.folder_name)
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-            return folder_path
-
-        count = 2
+        folder_count = 2
         while True:
-            new_folder_name = f'{self.folder_name}{count}'
-            new_folder_path = os.path.join(self.yolo_folder, new_folder_name)
-            if not os.path.exists(new_folder_path):
-                os.makedirs(new_folder_path)
-                return new_folder_path
-            count += 1
+            new_folder_name = f'{self.folder_name}{folder_count}'
+            folder = os.path.join(self.yolo_folder, new_folder_name)
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+                return folder
+            folder_count += 1
+    # ==============================================================================
 
-
-    def xml_to_yolo(self, xml_path, output_path):
-        tree = ET.parse(xml_path)
+    # Core
+    def xml_to_yolo(self, xml, yolo):
+        tree = ET.parse(xml)
         root = tree.getroot()
-
         image_width = int(root.find('size/width').text)
         image_height = int(root.find('size/height').text)
 
-        with open(output_path, 'w') as output_file:
+        with open(yolo, 'w') as write_yolo:
             for obj in root.findall('object'):
-                class_label = obj.find('name').text
-                if class_label not in self.class_mapping:
+                class_name = obj.find('name').text
+                if class_name not in self.classes:
                     continue
 
                 xmin = int(obj.find('bndbox/xmin').text)
@@ -45,20 +46,26 @@ class ConvertXML2YOLO:
                 xmax = int(obj.find('bndbox/xmax').text)
                 ymax = int(obj.find('bndbox/ymax').text)
 
+                # Yolo formating
                 x_center = (xmin + xmax) / (2.0 * image_width)
                 y_center = (ymin + ymax) / (2.0 * image_height)
                 width = (xmax - xmin) / image_width
                 height = (ymax - ymin) / image_height
 
-                output_file.write(f"{self.class_mapping[class_label]} {x_center} {y_center} {width} {height}\n")
-
-
+                write_yolo.write(f'{self.classes[class_name]} {x_center} {y_center} {width} {height}\n')
+    # ==============================================================================
+                
     def run(self):
-        count = 0
+        progress_count = 0
+        total_xml = len(os.listdir(self.xml_folder))
         for xml_file in os.listdir(self.xml_folder):
             if xml_file.endswith('.xml'):
                 xml_path = os.path.join(self.xml_folder, xml_file)
-                output_path = os.path.join(self.base_folder_path, xml_file.replace('.xml', '.txt'))
-                self.xml_to_yolo(xml_path, output_path)
-                count += 1
-        print(f'Done convert {count} labels!')
+                save = os.path.join(self.folder_path, xml_file.replace('.xml', '.txt'))
+                self.xml_to_yolo(xml_path, save)
+
+                # Print out progress bar
+                progress_count += 1
+                progress = int((progress_count / total_xml) * 40)
+                sys.stdout.write('\r[' + '.' * progress + ' ' * (40 - progress) + f'] {progress_count}/{total_xml}')
+                sys.stdout.flush()
