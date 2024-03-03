dbe2fe702e2f4e36b995954429157dfd2aba1366
Dhaboav <dhaboav@gmail.com>
2024-03-03 23:31:17 +0700

    Serial delay issue

--- komunikasi/ros_receiver_control.py
+++ komunikasi/ros_receiver_control.py
@@ -8,7 +8,6 @@ from rclpy.node import Node
 from ros_robot.komunikasi.client_side import Client
 
 
-
 class RosReceiverControl(Node):
     def __init__(self, com:str):
         super().__init__('Testing')
@@ -17,16 +16,18 @@ class RosReceiverControl(Node):
         self.__tester = self.create_timer(1.0, self.timer_callback)
 
     def timer_callback(self):
-        self.serial_arduino(self.__previous_msg)
+        self.get_logger().info(self.__previous_msg)
 
     def receiver_callback(self, msg:str):
         if self.__previous_msg != msg:
             self.__previous_msg = msg
-         
+            self.serial_arduino(msg)
+        else:
+            pass
+            
     def serial_arduino(self, msg:str):
         self.__serial_port.write(msg.encode())
-        self.get_logger().info(msg)
-
+        
 
 def main(args=None):
     current_folder = Path(__file__).resolve().parent
